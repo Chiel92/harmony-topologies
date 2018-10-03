0467197ebe5f229a6f0ba23e0daddcf7cc2a51c9
Chiel ten Brinke <ctenbrinke@gmail.com>
2018-10-03 23:31:20 +0200

    first experiment

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ Model assumptions:
 '''
 
 f = open('out.dot', 'w')
+
+
 def say(msg):
     print(msg, file=f)
+
 
 tones = [
     'C',
@@ -23,19 +26,65 @@ tones = [
     'B'
 ]
 
-def print_leittones():
-    for i,tone in enumerate(tones):
-        say('  "{}" -> "{}";'.format(tone, tones[i-1]))
+consonants = set()
+consonants.add((0, 0))
+progressions = set()
+progressions.add((0, 0))
 
-def print_quints():
-    for i,tone in enumerate(tones):
-        say('  "{}" -> "{}";'.format(tone, tones[i-5]))
+
+def add_consonants(tone_set):
+    for _, tone in list(tone_set):
+        consonants.add((tone, (tone - 4) % 12))
+        consonants.add((tone, (tone - 5) % 12))
+        consonants.add((tone, (tone - 7) % 12))
+
+
+def add_progressions(tone_set):
+    for _, tone in list(tone_set):
+        progressions.add((tone, (tone - 1) % 12))
+        progressions.add((tone, (tone - 5) % 12))
+
+
+def reachable_by_progression():
+    result = set()
+    for t1, t2 in progressions:
+        result.add(t1)
+        result.add(t2)
+    return result
+
+
+def reachable_by_consonance():
+    result = set()
+    for t1, t2 in consonants:
+        result.add(t1)
+        result.add(t2)
+    return result
+
+
+def print_consonants():
+    reachable = reachable_by_progression()
+    for t1, t2 in consonants:
+        if t1 != t2 and t1 in reachable and t2 in reachable:
+            say('  "{}" -> "{}" [dir=none,color=red];'.format(tones[t1], tones[t2]))
+
+
+def print_progressions():
+    reachable = reachable_by_consonance()
+    for t1, t2 in progressions:
+        if t1 != t2 and t1 in reachable and t2 in reachable:
+            say('  "{}" -> "{}";'.format(tones[t1], tones[t2]))
 
 
 def run():
+    add_progressions(progressions)
+    add_consonants(consonants)
+    add_progressions(consonants)
+    add_consonants(progressions)
+
     say('digraph harmony {')
-    print_leittones()
-    print_quints()
+    print_consonants()
+    print_progressions()
     say('}')
+
 
 run()
